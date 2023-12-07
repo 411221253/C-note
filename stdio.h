b811bc163457fc3eebdab98a6327a915de216201
411221253 <145320808+411221253@users.noreply.github.com>
2023-12-07 23:27:15 +0800

    Update stdio.h

--- stdio.h
+++ stdio.h
@@ -34,3 +34,44 @@ int main() {
 
     return 0;
 }
+
+
+//attribute
+
+#include <stdio.h>
+
+// 使用屬性指定變數對齊為16位元組
+int my_variable __attribute__((aligned(16)));
+
+int main() {
+    my_variable = 42;
+
+    // 在這裡可以使用my_variable，並且可以確保其對齊方式為16位元組
+
+    printf("The value of my_variable: %d\n", my_variable);
+
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 使用屬性指定函數不會返回
+void my_exit_function() __attribute__((noreturn));
+
+int main() {
+    printf("This is the main function.\n");
+
+    // 呼叫標記為noreturn的函數
+    my_exit_function();
+
+    // 這行程式碼永遠不會執行，因為my_exit_function標記為不會返回
+
+    return 0;
+}
+
+// 定義標記為noreturn的函數
+void my_exit_function() {
+    printf("This function will exit the program.\n");
+    exit(0);
+}
