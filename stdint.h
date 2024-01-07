af42082e11891bcfdf9efda2163e98070c5e9576
411221253 <145320808+411221253@users.noreply.github.com>
2024-01-07 21:24:05 +0800

    Create  stdint.h

---
+++ stdint.h
@@ -0,0 +1,41 @@
+typedef signed integer type int8_t;     // optional
+typedef signed integer type int16_t;    // optional
+typedef signed integer type int32_t;    // optional
+typedef signed integer type int64_t;    // optional
+typedef unsigned integer type uint8_t;  // optional
+typedef unsigned integer type uint16_t; // optional
+typedef unsigned integer type uint32_t; // optional
+typedef unsigned integer type uint64_t; // optional
+
+typedef signed integer type int_fast8_t;
+typedef signed integer type int_fast16_t;
+typedef signed integer type int_fast32_t;
+typedef signed integer type int_fast64_t;
+typedef unsigned integer type uint_fast8_t;
+typedef unsigned integer type uint_fast16_t;
+typedef unsigned integer type uint_fast32_t;
+typedef unsigned integer type uint_fast64_t;
+
+typedef signed integer type int_least8_t;
+typedef signed integer type int_least16_t;
+typedef signed integer type int_least32_t;
+typedef signed integer type int_least64_t;
+typedef unsigned integer type uint_least8_t;
+typedef unsigned integer type uint_least16_t;
+typedef unsigned integer type uint_least32_t;
+typedef unsigned integer type uint_least64_t;
+
+ptrdiff_t
+PTRDIFF_MIN 為 -65535
+PTRDIFF_MAX 為 +65535
+sig_atomic_t
+SIG_ATOMIC_MIN
+SIG_ATOMIC_MAX
+size_t
+SIZE_MAX 為 +65535
+wchar_t
+WCHAR_MIN
+WCHAR_MAX
+wint_t
+WINT_MIN
+WINT_MAX
